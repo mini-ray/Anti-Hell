2a5bfac9605278d914be7af7ce4b7dc78f546bf5
Seth Hadley <21.seth.hadley@ksd.org>
2021-02-11 10:56:02 -0800

    Adding more player code

--- Anti Hell/Assets/Scripts/Player.cs
+++ Anti Hell/Assets/Scripts/Player.cs
@@ -5,21 +5,29 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    //Config
     [SerializeField] float runSpeed = 5f;
-
+    [SerializeField] float jumpSpeed = 5f;
+    //State
+    bool isAlive = true;
+    //Cached conponent references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2d;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
-        
+        myAnimator = GetComponent<Animator>();
+        myCollider2d = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
         
     }
@@ -29,6 +37,22 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+    private void Jump()
+    {
+        if (!myCollider2d.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
